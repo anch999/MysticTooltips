c0a4f4e6e4d995e8932b92ed378dc12785f98298
Anch <adrianpratt.ap@gmail.com>
2022-07-01 02:41:50 +1200

    remove sync batch file

--- folder sync.bat
+++
@@ -1,3 +0,0 @@
-mklink /J "E:\Ascension Launcher\resources\client\Interface\AddOns\MysticTooltips" "D:\Addon Dev\Git Hub\MysticTooltips\MysticTooltips"
-
-mklink /J "E:\Ascensionclient\Interface\AddOns\MysticTooltips" "D:\Addon Dev\Git Hub\MysticTooltips\MysticTooltips"
